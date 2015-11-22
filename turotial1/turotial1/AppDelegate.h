3e06b9cc24969b977285a1b46d71c7275458077e
kalyan chakravarthy <kalyan711987@gmail.com>
2015-11-22 18:40:20 +0530

    first commit

---
+++ turotial1/turotial1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  turotial1
+//
+//  Created by kalyan chakravarthy on 22/11/15.
+//  Copyright © 2015 KalyanOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ turotial1/turotial1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  turotial1
+//
+//  Created by kalyan chakravarthy on 22/11/15.
+//  Copyright © 2015 KalyanOrg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
